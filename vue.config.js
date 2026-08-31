b6b37f82bd0ea2ef7342bb7874941e23b7c17e17
TheStami <a15@int.pl>
2026-08-31 20:00:24 +0200

    Fix publicPath for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/TAURI-akochandaaaaaa/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/akochandaaaaaa-desktop/" : "/",
   outputDir: "dist/",
   pwa: {
     name: 'Mahjong Calculator',
